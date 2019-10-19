8632ec01a6e1bcd1ca78926e3ef60c515854e804
ilabstaff <ilabstaff@172.16.116.68>
2019-10-19 10:56:53 -0700

    random

--- src/_01_intro_to_random/IntroToRandom.java
+++ src/_01_intro_to_random/IntroToRandom.java
@@ -5,25 +5,30 @@ package _01_intro_to_random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
 		int num = 0;
 		
 		//3   Now to make num random. 
+		   
+		
 		//3a. Create an object of the Random class, Hint: Random ran = new Random()
-		
+		    Random ran = new Random();
+		    
 		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
-		
+		      num=ran.nextInt();
 
 		//4. Now limit the value of num to be between 0 and 100
-
+               num=ran.nextInt(101);
 		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
 
 		//6. Challenge: Limit the value of num to be between -222 and 88
 
 		//1. Print out the value of num
-		
+		     JOptionPane.showMessageDialog(null, num);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
